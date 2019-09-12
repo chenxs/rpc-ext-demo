2460c6db889aa6f2e25caadbad459e3701257890
hillchen <chenxiaoshan@duiba.com.cn>
2019-09-12 15:01:40 +0800

    添加手动生成指定Annotation对象,annotation对象转换，将接口上带RpcApi注解的实现暴露到服务注册中心

--- rpc-ext-demo-dubbo/rpc-ext-demo-dubbo-api/src/main/java/cn/hill4j/rpcext/demo/dubbo/api/SayHello.java
+++ rpc-ext-demo-dubbo/rpc-ext-demo-dubbo-api/src/main/java/cn/hill4j/rpcext/demo/dubbo/api/SayHello.java
@@ -1,11 +1,14 @@
 package cn.hill4j.rpcext.demo.dubbo.api;
 
+import cn.hill4j.rpcext.core.rpcext.dubbo.annotation.RpcApi;
+
 /**
  * 2019/8/28 19:31 <br>
  * Description: SayHello
  *
  * @author hillchen
  */
+@RpcApi
 public interface SayHello {
     /**
      * 测试方法
